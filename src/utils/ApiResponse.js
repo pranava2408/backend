82d714cb6a386f1ada8aadd0bb24c422d4cd11a1
Balla Pranava Chaitanya <pranavachaitanyaballa@gmail.com>
2026-06-08 17:00:24 +0530

    fixing the mistake

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,5 +1,5 @@
 class ApiResponse{
-    constructor(statusCode, data,message="Success"*){
+    constructor(statusCode, data,message="Success"){
         this.statusCode = statusCode;
         this.message = message;
         this.success = statusCode<400;
